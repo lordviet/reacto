824704ace8b32a52f763cbef17371c2f6c6da9d4
lordviet <viet1231@abv.bg>
2020-03-17 02:45:53 +0200

    create board initial

--- client/src/Boards/Board/Board.jsx
+++ client/src/Boards/Board/Board.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Board.css'
 
 function Board(props) {
+    
+    // Change the hook whenever the board is clicked
+    const [active, setActive] = useState(false);
+    if (props.name) {
+        return (
+            <div className="board">
+                <h2>{props.name}</h2>
+            </div>
+        )
+    }
+    
+    console.log(active);
+    // create a var and change it according to active 
+    // className create-board-active
     return (
-        <div className="board">
-            {props.name ? <h2>{props.name}</h2> : <h2>Create new board...</h2>}
+
+        <div className="board create-board"
+            onClick={() => setActive(true)}>
+            <h2>Create new board...</h2>
         </div>
     )
 }
