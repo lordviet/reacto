67e73a1ef58b1ec8de21d72f74f920b39aced435
lordviet <viet1231@abv.bg>
2020-03-18 02:35:45 +0200

    create board UI

--- client/src/Boards/Board/Board.jsx
+++ client/src/Boards/Board/Board.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from 'react'
 import './Board.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
 
 function Board(props) {
-    
-    // Change the hook whenever the board is clicked
+
     const [active, setActive] = useState(false);
+    const [boardName, setBoardName] = useState('');
+    const [error, showError] = useState(false);
+
+    function closeBoard() {
+        setActive(false);
+        setBoardName('');
+        showError(false);
+    }
+
+    function createBoard() {
+        if (boardName) {
+            closeBoard();
+            console.log(boardName);
+        }
+        else{
+            showError(true);
+        }
+
+    }
+
     if (props.name) {
         return (
             <div className="board">
@@ -12,17 +34,32 @@ function Board(props) {
             </div>
         )
     }
-    
-    console.log(active);
-    // create a var and change it according to active 
-    // className create-board-active
-    return (
 
-        <div className="board create-board"
+    let board;
+
+    // TODO should be form
+    if (active) {
+        board = <div className="board create-board create-board-active">
+            <FontAwesomeIcon className="closeIcon" icon={faTimes} onClick={() => closeBoard()} />
+            <h3>Creating new board</h3>
+            <p>What shall we call the board?</p>
+            <input type="text" name="boardname" onChange={e => setBoardName(e.target.value)} />
+            <button onClick={() => createBoard()}>CREATE</button>
+            <p className={error ? "create-board-err" : "create-board-err hidden"}>Oops! You forgot the name!</p>
+        </div>
+    }
+
+    else {
+        board = <div className="board create-board"
             onClick={() => setActive(true)}>
             <h2>Create new board...</h2>
         </div>
+    }
+
+    return (
+        board
     )
 }
+
 
 export default Board;
