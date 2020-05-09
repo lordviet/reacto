5909f2c6d9b19d857fbe2df365361cf9d9e9567f
lordviet <viet1231@abv.bg>
2020-05-09 17:17:09 +0300

    add max length name error

--- client/src/Boards/Board/Board.jsx
+++ client/src/Boards/Board/Board.jsx
@@ -9,22 +9,25 @@ function Board(props) {
 
     const [active, setActive] = useState(false);
     const [boardName, setBoardName] = useState('');
-    const [error, showError] = useState(false);
+    const [error, showError] = useState('');
+
 
     function closeBoard() {
         setActive(false);
         setBoardName('');
-        showError(false);
+        showError('');
     }
 
     function createBoard() {
-        if (boardName) {
+        if (boardName && boardName.length <= 30) {
             closeBoard();
             boardActions.addBoardToStorage(boardName);
             props.updateBoards(JSON.parse(localStorage.getItem("boards")));
         }
         else {
-            showError(true);
+            boardName.length > 30 ?
+                showError("Name is too long!") :
+                showError('Oops! You forgot the name!')
         }
 
     }
@@ -50,7 +53,7 @@ function Board(props) {
             <p>What shall we call the board?</p>
             <input type="text" name="boardname" onChange={e => setBoardName(e.target.value)} />
             <button onClick={() => createBoard()}>CREATE</button>
-            <p className={error ? "create-board-err" : "create-board-err hidden"}>Oops! You forgot the name!</p>
+            <p className={error ? "create-board-err" : "create-board-err hidden"}>{error}</p>
         </div>
     }
 
