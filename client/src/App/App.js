a5220debbf1a17be7869b2a488dee0cddfe4a75c
lordviet <viet1231@abv.bg>
2020-05-09 05:28:35 +0300

    added react router

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import Boards from '../Boards/Boards';
+import BoardLists from '../BoardLists/BoardLists'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Boards/>
+      <Router>
+        <Switch>
+          <Route path='/' component={Boards} />
+          <Route path='/board/:id' component={BoardLists} />
+        </Switch>
+      </Router>
     </div>
   );
 }
